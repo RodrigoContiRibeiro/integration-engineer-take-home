391ab85b5bae2b7c0bb5478e455e007c407eb29c
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2024-11-27 19:17:39 -0300

    feat(cors): adding cors middleware

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 let tasks = [];
 let nextTaskId = 1;
 
-app.get('/tasks', (req, res) => {
+app.get("/tasks", (req, res) => {
   res.json(tasks);
 });
 
-app.post('/tasks', (req, res) => {
-});
+app.post("/tasks", (req, res) => {});
 
-
-app.delete('/tasks/:id', (req, res) => {
-});
+app.delete("/tasks/:id", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
