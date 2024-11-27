716f5cfbbc761df404e94ee5f7f0e03ae8204b29
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2024-11-27 20:15:47 -0300

    feat(frontend/tasks): implementing creating and deletion

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,54 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 type Task = {
-  id: number,
-  title: string,
-  description: string
-}
+  id: string;
+  title: string;
+  description: string;
+};
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>([]);
-  const [formData, setFormData] = useState({ title: '', description: '' });
+  const [formData, setFormData] = useState({ title: "", description: "" });
 
   useEffect(() => {
     fetchTasks();
   }, []);
 
   const fetchTasks = async () => {
-    const response = await fetch('http://localhost:8000/tasks')
+    const response = await fetch("http://localhost:8000/tasks");
     const tasks = await response.json();
     setTasks(tasks);
   };
 
-  /* Complete the following functions to hit endpoints on your server */
   const createTask = async () => {
+    const response = await fetch("http://localhost:8000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const newTask = await response.json();
+    setTasks((prev) => {
+      return [...prev, newTask];
+    });
   };
 
   const deleteTask = async (id: string) => {
-  };
+    const response = await fetch(`http://localhost:8000/tasks/${id}`, {
+      method: "DELETE",
+    });
 
+    const newTasks = await response.json();
+
+    setTasks(newTasks);
+  };
 
   return (
     <div>
       <h1>Task Management App</h1>
       <ul>
-        {tasks.map(task => (
+        {tasks.map((task) => (
           <li key={task.id}>
             <h3>{task.title}</h3>
             <p>{task.description}</p>
@@ -46,13 +62,15 @@ function App() {
           type="text"
           placeholder="Title"
           value={formData.title}
-          onChange={e => setFormData({ ...formData, title: e.target.value })}
+          onChange={(e) => setFormData({ ...formData, title: e.target.value })}
         />
         <input
           type="text"
           placeholder="Description"
           value={formData.description}
-          onChange={e => setFormData({ ...formData, description: e.target.value })}
+          onChange={(e) =>
+            setFormData({ ...formData, description: e.target.value })
+          }
         />
         <button onClick={createTask}>Create</button>
       </div>
