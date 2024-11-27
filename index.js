6289b22b3feec4095d0a591a44e32ae50c9c37f5
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2024-11-27 20:01:26 -0300

    feat(tasks): initial crud

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
+const { randomUUID } = require("crypto");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -13,15 +14,83 @@ app.use(helmet());
 app.disable("x-powered-by");
 
 let tasks = [];
-let nextTaskId = 1;
+
+const tasksService = {
+  listTasks: () => {
+    return tasks;
+  },
+  createTask: ({ title, description }) => {
+    const newTask = {
+      id: randomUUID(),
+      title,
+      description,
+    };
+
+    tasks.push(newTask);
+
+    return newTask;
+  },
+  deleteTask: (id) => {
+    const filteredTasks = tasks.filter((task) => task.id !== id);
+
+    tasks = filteredTasks;
+
+    return tasks;
+  },
+  updateTask: (id, newFields) => {
+    const newTasks = tasks.map((task) => {
+      const isTaskToBeUpdated = task.id === id;
+      if (isTaskToBeUpdated) {
+        const updatedTask = {
+          ...task,
+          ...newFields,
+        };
+
+        return updatedTask;
+      }
+
+      return task;
+    });
+
+    tasks = newTasks;
+
+    return tasks;
+  },
+};
 
 app.get("/tasks", (req, res) => {
+  const tasks = tasksService.listTasks();
+
   res.json(tasks);
 });
 
-app.post("/tasks", (req, res) => {});
+app.post("/tasks", (req, res) => {
+  const taskFields = req.body;
 
-app.delete("/tasks/:id", (req, res) => {});
+  const newTask = tasksService.createTask(taskFields);
+
+  res.json(newTask);
+});
+
+app.patch("/tasks/:id", (req, res) => {
+  const taskIdToBeUpdated = req.params.id;
+  const taskFieldsToBeUpdated = req.body;
+
+  const newTasks = tasksService.updateTask(
+    taskIdToBeUpdated,
+    taskFieldsToBeUpdated
+  );
+
+  res.json(newTasks);
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  const taskIdToBeDeleted = req.params.id;
+
+  const newTasks = tasksService.deleteTask(taskIdToBeDeleted);
+
+  res.json(newTasks);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
