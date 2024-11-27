c5f599eba84f36a58833300863db75598f6f9710
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2024-11-27 19:32:34 -0300

    feat(security): applying express' docs security best practices

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const helmet = require("helmet");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(helmet());
+
+app.disable("x-powered-by");
 
 let tasks = [];
 let nextTaskId = 1;
@@ -21,4 +25,15 @@ app.delete("/tasks/:id", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// custom 404
+app.use((req, res, next) => {
+  res.status(404).send("Oopsie daisy, page not found >.<");
+});
+
+// custom error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something exploded?!");
 });
