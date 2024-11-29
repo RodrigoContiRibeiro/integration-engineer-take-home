5bb9d894f5b97a9ba8c43ee187f022ccfebcd63b
RodrigoContiRibeiro <rodrigo.conti.ribeiro@gmail.com>
2024-11-29 13:51:19 -0300

    feat(frontend/table): adding loading for the delete handler

--- frontend/src/components/tasks-table/delete-task-button.tsx
+++ frontend/src/components/tasks-table/delete-task-button.tsx
@@ -3,14 +3,24 @@ import { IconButton } from "@radix-ui/themes";
 import { TrashIcon } from "@radix-ui/react-icons";
 
 export type DeleteTaskButtonProps = {
-  onClick: (evt: React.MouseEvent<HTMLButtonElement>) => void;
+  onClick: (evt: React.MouseEvent<HTMLButtonElement>) => Promise<void>;
 };
 
 export const DeleteTaskButton: React.FC<DeleteTaskButtonProps> = ({
   onClick,
 }) => {
+  const [loading, setLoading] = React.useState<boolean>(false);
+
+  const clickHandler = async (evt: React.MouseEvent<HTMLButtonElement>) => {
+    setLoading(true);
+
+    await onClick(evt);
+
+    setLoading(false);
+  };
+
   return (
-    <IconButton color="red" onClick={onClick}>
+    <IconButton color="red" onClick={clickHandler} loading={loading}>
       <TrashIcon width={16} height={16} />
     </IconButton>
   );
